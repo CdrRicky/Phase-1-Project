cf06b22de6e100b40193734c1c3c105181bc60af
Dev_Ricky <eric.kiogora12@gmail.com>
2022-06-26 13:50:36 +0300

    javascript

--- script.js
+++ script.js
@@ -59,10 +59,14 @@ fetch(url)
                 formClose.addEventListener('click', () =>{
                   loginForm.classList.remove('active');
               });
-              
-                    
-                    
-                  
+              videoBtn.forEach(btn =>{
+                btn.addEventListener('click', ()=>{
+                    document.querySelector('.controls .active').classList.remove('active');
+                    btn.classList.add('active');
+                    let src = btn.getAttribute('data-src');
+                    document.querySelector('#video-slider').src = src;
+                });
+            });                    
                   var swiper = new Swiper(".review-slider", {
                       spaceBetween: 20,
                       loop:true,
